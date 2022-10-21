d3ca47c9276a3b0b48ed0e882d5c9b850840f5c2
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-21 21:45:52 +0300

    Добавьте файлы проекта.

---
+++ Lesson 8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\yxakr\Desktop\California";
+            string[] allfiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string file in allfiles)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
